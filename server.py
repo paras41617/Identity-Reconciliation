b4334317c9c6aafc0b19ccbe3bc55f43a6bdec70
paras41617 <paras41617@gmail.com>
2024-07-10 18:49:14 +0530

    feature: identify endpoint with handling when one among email and phone is provided

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -30,13 +30,82 @@ def identify_contact():
     email = data.get('email')
     phoneNumber = data.get('phoneNumber')
 
-    response_data = {
-        "email": email,
-        "phoneNumber": phoneNumber,
-    }
+    with app.app_context():
 
-    return response_data
+        def get_primary_contact_by_email(email):
+            primary_contact = Contact.query.filter_by(email=email, linkPrecedence='primary').first()
+            if not primary_contact:
+                secondary_contact = Contact.query.filter_by(email=email).first()
+                if secondary_contact:
+                    primary_contact = Contact.query.filter_by(id=secondary_contact.linkedId).first()
+            return primary_contact
 
+        def get_primary_contact_by_phone(phone_number):
+            primary_contact = Contact.query.filter_by(phoneNumber=phone_number, linkPrecedence='primary').first()
+            if not primary_contact:
+                secondary_contact = Contact.query.filter_by(phoneNumber=phone_number).first()
+                if secondary_contact:
+                    primary_contact = Contact.query.filter_by(id=secondary_contact.linkedId).first()
+            return primary_contact
+
+        def gather_contacts(primary_contact):
+            contacts = Contact.query.filter_by(linkedId=primary_contact.id).all()
+            contacts.append(primary_contact)
+            return contacts
+
+        def prepare_response_data(contacts):
+            primary_contact = None
+            secondary_contacts = []
+            emails = []
+            phoneNumbers = []
+            secondaryContactIds = []
+
+            for contact in contacts:
+                if contact.linkPrecedence == "primary":
+                    primary_contact = contact
+                    if contact.email and contact.email not in emails:
+                        emails.insert(0, contact.email)
+                    if contact.phoneNumber and contact.phoneNumber not in phoneNumbers:
+                        phoneNumbers.insert(0, contact.phoneNumber)
+                else:
+                    secondary_contacts.append(contact)
+                    if contact.email and contact.email not in emails:
+                        emails.append(contact.email)
+                    if contact.phoneNumber and contact.phoneNumber not in phoneNumbers:
+                        phoneNumbers.append(contact.phoneNumber)
+                    secondaryContactIds.append(contact.id)
+
+            response_data = {
+                "primaryContactId": primary_contact.id if primary_contact else None,
+                "emails": emails,
+                "phoneNumbers": phoneNumbers,
+                "secondaryContactIds": secondaryContactIds
+            }
+
+            return response_data
+
+        primaryContract = None
+        contacts = []
+
+        if email and not phoneNumber:
+            primaryContract = get_primary_contact_by_email(email)
+                
+        elif phoneNumber and not email:
+            primaryContract = get_primary_contact_by_phone(phoneNumber)
+
+        elif email and phoneNumber:
+            pass
+        
+        if primaryContract:
+            contacts = gather_contacts(primaryContract)
+
+        if contacts:
+            response_data = {
+                "contact": prepare_response_data(contacts)
+            }
+            return jsonify(response_data), 200
+        else:
+            return jsonify({"message": "No contact found"}), 404
 
 if __name__ == '__main__':
     app.run()
